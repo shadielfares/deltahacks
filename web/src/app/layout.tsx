dd64b07ed15f20801412e17319ae961a5b233465
Nimay Desai <62720666+NimayDesai@users.noreply.github.com>
2025-01-11 18:49:34 -0500

    Remove useless files in .history
    Co-authored-by: Kevin Liang <k-liang9@users.noreply.github.com>
    Co-authored-by: Vincent Nguyen <vinny-nguyen@users.noreply.github.com>

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Navbar from "./components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navbar />
           {children}
         </body>
       </UserProvider>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Image from "next/image";
 
 export default function ProfileClient() {
   const { user, error, isLoading } = useUser();

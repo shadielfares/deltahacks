36534f1e4e39ad0e90146b86768ff8cd263283ae
shadielfares <shadi.elfares@gmail.com>
2025-01-11 17:56:56 -0500

    Added Navbar, patient-list (needs to be completed), home page, and logout button

---
+++ web/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../navbar/page";
+
+export default function HomePage() {
+  return (
+    // insert the logic for all the components here
+    <div></div>
+  );
+}
---
+++ web/src/app/logout/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LogoutContainer = ({ name }: { name: string }) => {
+  const handleSignOut = () => {
+    console.log(`${name} signed out.`);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-[#292727] text-sm font-normal font-sans">
+        Logged in as {name}
+      </h1>
+      <button
+        onClick={handleSignOut}
+        className="w-full h-[52px] p-[17px] bg-[#e9efec] rounded-[10px] justify-center items-center gap-2.5 inline-flex overflow-hidden"
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default LogoutContainer;

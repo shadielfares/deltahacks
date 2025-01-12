3e64fa71c9e26eaf3b26816641a939d8a4a8ee83
Nimay Desai <62720666+NimayDesai@users.noreply.github.com>
2025-01-11 20:46:26 -0500

    Remove usless js files

--- server/dist/index.js
+++
@@ -1,5 +0,0 @@
-const main = async () => { };
-main().catch((err) => {
-    console.error(err);
-});
-//# sourceMappingURL=index.js.map
--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/login")
-def login():
-    return {"message": "Login"}
+@app.get("/upload-data")
+async def upload_data():
+    return {"message": "Data uploaded successfully!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
